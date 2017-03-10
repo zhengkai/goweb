37b656669203b5ca9e98bf08a84d84fd0575bd39
Zheng Kai <zhengkai@gmail.com>
2017-03-10 18:37:32 +0800

    up

--- handle/abc.go
+++ handle/abc.go
@@ -19,7 +19,6 @@ type Abc struct {
 func (this Abc) New() handle.Handle {
 	c := this
 	c.Head = new(CommonHead).New()
-	// c.Head.AddJS(`/res/script.js`)
 	return &c
 }
 
@@ -28,15 +27,6 @@ func (this *Abc) Get(w http.ResponseWriter, r *http.Request) {
 
 	data[`Foo`] = atomic.AddInt64(&connectCount, 1)
 	data[`Bar`] = 123
-
-	// fmt.Println(`this.Head`, this.Head)
-	this.Head.AddJS(`/res/script.js`)
-	this.Head.AddJS(`/res/go.js`)
-
-	this.Head.AddCSS(`/res/yes.css`)
-	this.Head.AddCSS(`/res/a.less`)
-	this.Head.AddCSS(`/res/a.css`)
-	this.Head.AddCSS(`/res/b.less`)
 
 	this.Data = data
 }
--- handle/common_head.go
+++ handle/common_head.go
@@ -10,6 +10,13 @@ type CommonHead struct {
 
 func (this CommonHead) New() handle.Head {
 	c := this.BaseHead.New()
-	c.AddMeta(`<meta name="theme-color" content="#375EAB">`)
+	c.AddJS(`/res/less.2.7.2.min.js`)
+	c.AddJS(`/res/jquery-3.1.1/jquery.min.js`)
+	c.AddJS(`/res/tether-1.3.3/js/tether.min.js`)
+	c.AddJS(`/res/bootstrap-4.0.0-alpha.6/js/bootstrap.js`)
+	c.AddCSS(`/res/bootstrap-4.0.0-alpha.6/css/bootstrap.min.css`)
+	c.AddCSS(`/res/tether-1.3.3/css/tether.min.css`)
+	c.AddCSS(`/res/font-awesome-4.7.0/css/font-awesome.min.css`)
+	c.AddCSS(`/res/style.less`)
 	return c
 }
